4910074e27f80828da1d7db5ec11d8727ba89b52
vartom <babanakov_dima@mail.ru>
2018-05-13 01:19:02 +0700

    mmc hoct sdhci-tegra: Fix NVQUIRK for HS add NVQUIRK_ENABLE_SDHCI_SPEC_300

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -438,8 +438,8 @@ static const struct sdhci_pltfm_data sdhci_tegra210_pdata = {
 
 static const struct sdhci_tegra_soc_data soc_data_tegra210 = {
 	.pdata = &sdhci_tegra210_pdata,
-	.nvquirks = NVQUIRK_ENABLE_SDR50 |
-		    NVQUIRK_ENABLE_DDR50 |
+	.nvquirks = NVQUIRK_ENABLE_SDHCI_SPEC_300 |
+		    NVQUIRK_ENABLE_SDR50 |
 		    NVQUIRK_ENABLE_SDR104 |
 		    NVQUIRK_HAS_PADCALIB |
 		    NVQUIRK_NEEDS_PAD_CONTROL,
